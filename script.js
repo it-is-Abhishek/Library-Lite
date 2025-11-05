a77262de3dd1551afb74bdc2f357e2174e3bee1e
it-is-Abhishek <abhishekverma1489@gmail.com>
2025-11-05 14:00:23 +0530

    initialize GSAP &  Lenis configuration for scroll-based animations

---
+++ script.js
@@ -0,0 +1,15 @@
+import { time } from "framer-motion";
+import gsap from "gsap";
+import {ScrollTrigger} from "gsap/ScrollTrigger";
+import Lenis from "lenis";
+
+document.addEventListener("DOMContentLoaded", () => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const lenis = new Lenis();
+    lenis.on("scroll", ScrollTrigger.update);
+    gsap.ticker.add((time) => {
+        lenis.raf(time * 1000);
+    });
+    gsap.ticker.lagSmoothing(0);
+});
